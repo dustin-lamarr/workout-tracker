367c6268c5eda90b2f16250c23ab2be27c9a3f43
Dustin LaMarr <dustin.lamarr@gmail.com>
2021-01-26 14:31:54 -0700

    Troubleshooting heroku deploy

--- Develop/routes/api/apiRoutes.js
+++ Develop/routes/api/apiRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const Workout = require('../../models/Workouts.js');
+const Workout = require('../../models/');
 
 router.get('/workouts', async (req,res) => {
     console.log("made it to api/workouts")
